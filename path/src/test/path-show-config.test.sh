2daea86c14ade771807d9bebb353c3335d369f75
isaias-b <isaias.b@gmx.de>
2016-02-14 11:08:06 +0100

    Add test case to show current bug

---
+++ path/src/test/path-show-config.test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $(dirname $0)
+
+source ../main/path.sh
+
+expected=$PATH
+expected=$expected:$(realpath .bin)
+expected=$expected:$(realpath bin)
+expected=$expected:$(realpath subfolder/bin)
+
+echo $expected | sed 's/:/\n/g' > $0.expect~
+path show-config > $0.output~
+
+
